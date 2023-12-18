da7008173ae71ece76fa62db0918ed4cad9b659a
Pradumm <kpradumm@gmail.com>
2023-12-18 12:11:45 +0530

    todo app using react context-api

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,20 @@
+
+
+import React, { createContext, useContext } from "react";
+
+
+export const TodoContext = createContext({
+   
+    addTodo: (todo) => { },
+    updateTodo: (id, todo) => { },
+    deleteTodo: (id) => { },
+    toggleComplete: (id) => { }
+})
+
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+
+export const TodoProvider = TodoContext.Provider
